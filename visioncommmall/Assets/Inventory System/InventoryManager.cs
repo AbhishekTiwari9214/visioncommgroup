c312c0216433286dadc434bb711846b5a231f99d
Abhishek Tiwari <abhishektiwari.cs19@aryacollege.in>
2022-12-29 21:18:25 +0530

    Revert "Merge pull request #25 from AbhishekTiwari9214/hiteshnew"
    This reverts commit d1c2a5d579e170a6089530a495d15a81bfa68a82, reversing
    changes made to 7af84c0a0a4a091416dbeed2ab9f5f54f1501e72.

--- visioncommmall/Assets/Inventory System/InventoryManager.cs
+++ visioncommmall/Assets/Inventory System/InventoryManager.cs
@@ -72,7 +72,7 @@ if(Items.Count==0){
   Total.text=$"{sumtotal}";
 }
    }
-/////////////////////////////////////////////////////////////////////
+
 public void EnableItemsRemove()
  {
  if (EnableRemove.isOn)
